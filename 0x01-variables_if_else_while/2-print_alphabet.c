0bdf9b48474234bd0534983040c5bbeea2c62929
root <palisoandisiwe@gmail.com>
2022-03-11 16:12:28 -0800

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; c++)
+	{
+		putchar(alpha);
+		putchar(\n);
+	}
+	return (0);
+}
