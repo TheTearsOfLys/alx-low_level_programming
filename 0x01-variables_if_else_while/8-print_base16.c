675c30aced598f35a9e47917b0a3d55a13b3423a
root <palisoandisiwe@gmail.com>
2022-03-11 18:35:10 -0800

    Base 16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits of base 16 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= 9; digit++)
+	{
+		putchar(digit);
+	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+	{
+		putcher(digit);
+	}
+	putchar('\n');
+	return (0);
+}
