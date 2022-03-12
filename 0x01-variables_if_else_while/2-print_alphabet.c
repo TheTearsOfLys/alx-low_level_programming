391629b4509afffc98dfc84b1be57f1b1e038f72
root <palisoandisiwe@gmail.com>
2022-03-11 16:17:58 -0800

    Lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,9 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha+)
-	{ putchar(alpha);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
 		putchar(\n);
 	}
 	return (0);
