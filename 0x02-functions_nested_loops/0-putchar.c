8515c3e27caf791c1e314999c0e0431b32da8eba
root <palisoandisiwe@gmail.com>
2022-03-15 03:16:59 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the word _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char name[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
+	return (0);
+}
