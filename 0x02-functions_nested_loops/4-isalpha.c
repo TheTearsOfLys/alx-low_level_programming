752f6b48e8ecb5d6917ddcad1cb4e802eb21ad11
root <palisoandisiwe@gmail.com>
2022-03-15 16:43:30 -0700

    Find alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet characters, lowercase or uppercase
+ * @c: input
+ * Return: 1 if alphabet, 0 if not
+ */
+
+int _isalpha.c(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
