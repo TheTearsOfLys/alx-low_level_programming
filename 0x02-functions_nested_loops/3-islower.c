7cd46f49e02a6475f41c23c55a0bae62b0c8912c
root <palisoandisiwe@gmail.com>
2022-03-15 16:28:18 -0700

    Find lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1when it finds lowercase character otherwise it returns 0
+ *
+ * Return: 1 for lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n');
+}
