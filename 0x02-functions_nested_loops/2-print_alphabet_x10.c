a82efe66584644252b9a73cbc48ee1e106f0531d
root <palisoandisiwe@gmail.com>
2022-03-15 15:50:46 -0700

    Print alphabets in lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,17 @@
 void main(void)
 {
 	int i = 0;
-	int alphabet = 97;
+	int alphabet;
 
 	while (i < 10)
 	{
-	for (alphabet = 97; alphabet < 123; alphabet++)
+	for (alphabet = 97; alphabet <= 122; alphabet++)
 	{
 		_putchar(alphabet);
 	}
+
 	i++;
+
 	}
 	_putchar('\n');
 }
