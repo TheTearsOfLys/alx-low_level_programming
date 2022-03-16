d4e42b781a0264e7be291d239ddc6e494e39e87a
unknown <palisoandisiwe@gmail.com>
2022-03-16 02:37:18 +0200

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,17 @@
 */
 int main(void)
 {
+        times_table();
+        return (0);
+n.h"
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
 	times_table();
 	return (0);
 }
