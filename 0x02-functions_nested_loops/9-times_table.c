8d8a457aa99f748248f6592aef1260a1167edc43
unknown <palisoandisiwe@gmail.com>
2022-03-16 02:45:39 +0200

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,33 @@
 #include "main.h"
 
 /**
- *print_sign - sign of a number.
- *@n: input.
- *
- *Return: 1 and print + if n > 1
- * 0 and print 0 if n = 0
- * -1 and print - if n < 0
- */
-int print_sign(int n)
+* times_table - that prints the 9 times table
+*
+* Return: no return
+*/
+void times_table(void)
 {
-int result;
-if (n > 0)
-{
-_putchar('+');
-result = 1;
-}
-if (n == 0)
-{
-_putchar('0');
-result = 0;
-}
-if (n < 0)
-{
-_putchar('-');
-result = -1;
-}
-return (result);
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+	_putchar('\n');
+	}
 }
