d73fff4e8de7e3cf7cb1889bc91ac4065990a5ce
root <palisoandisiwe@gmail.com>
2022-03-15 17:03:16 -0700

    Print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,12 @@
 /**
  * print_sign - prinyts the sign of an integer
  *@n: input
- * Return: 1 for positive and -1 for negative, 0 for zero 
+ * Return: 1 for positive and -1 for negative, 0 for zero
  */
 
 int print_sign(int n)
 {
-	if(n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
