1d826cac822c5f3d1a12ac7c25c886109c380cf3
unknown <palisoandisiwe@gmail.com>
2022-03-16 02:42:13 +0200

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(void)
+ *print_sign - sign of a number.
+ *@n: input.
+ *
+ *Return: 1 and print + if n > 1
+ * 0 and print 0 if n = 0
+ * -1 and print - if n < 0
+ */
+int print_sign(int n)
 {
-        times_table();
-        return (0);
-n.h"
-
-/**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(void)
+int result;
+if (n > 0)
 {
-	times_table();
-	return (0);
+_putchar('+');
+result = 1;
+}
+if (n == 0)
+{
+_putchar('0');
+result = 0;
+}
+if (n < 0)
+{
+_putchar('-');
+result = -1;
+}
+return (result);
 }
