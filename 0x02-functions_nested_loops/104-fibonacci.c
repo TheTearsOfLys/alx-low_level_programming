cadb8839baa357134b6c29be436009d2d9e94bda
unknown <palisoandisiwe@gmail.com>
2022-03-16 03:16:28 +0200

    print first 98 numbers of the fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 
 /**
- *main - print first 50 fibonacci
- *
- *Return: 0 always.
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
+ * Return: 0
  */
+
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	for (i = 0; i < 97; i++)
+	printf("1");
+
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		printf("%lu, ", x);
-		sum = x + y;
-		x = y;
-		y = sum;
-		if (i == 96)
-			printf("%lu\n", x);
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
 	}
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
+	printf("\n");
+
 	return (0);
 }
