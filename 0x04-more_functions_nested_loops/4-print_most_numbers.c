6276a844f3c1733de9d7f6a2dbf99d282566d45c
unknown <palisoandisiwe@gmail.com>
2022-03-17 20:41:08 +0200

    update files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,10 @@ void print_most_numbers(void);
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 || num == 4)
+		if (num != 2 || num != 4)
 		{
-			num++;
+			_putchar(num);
 		}
-		_putchar('0' + num);
 	}
 	_putchar('\n');
 }
