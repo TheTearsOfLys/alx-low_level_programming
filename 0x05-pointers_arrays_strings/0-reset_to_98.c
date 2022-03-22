459fb1e3391e3274dea2f0307a508676c834563c
TheTearsOfLys <palisoandisiwe@gmail.com>
2022-03-22 17:03:34 +0200

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- *reset_to_98 - takes a pointer to an int as a parameter and updates the value it points to to ninety eight
+ * reset_to_98 - takes a pointer to an int as a
+ * parameter and updates the value it points
+ * to to ninety eight
  * @n : pointer
  * Return: 0
  */
