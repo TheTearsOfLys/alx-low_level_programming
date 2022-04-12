d3edd023f860c67dbefaf79737b50b53325e6ced
TheTearsOfLys <palisoandisiwe@gmail.com>
2022-04-12 16:42:40 +0200

    Structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE dog.h
+#ifndef STRTYP
+#define STRTYP 
 
 /**
  * struct dog - Define a new struct dog with name, age, owner
@@ -16,7 +16,7 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#end
+
+#endif 
